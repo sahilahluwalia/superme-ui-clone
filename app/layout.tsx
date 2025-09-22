cabaeeefa81871859c58d93583e6c5d25a65a160
Sahil Ahluwalia <iamsahilahluwalia@gmail.com>
2025-09-22 21:39:48 +0530

    add: github button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Toolhouse Developer Relations Agent",
-  description: "AI-powered chat assistant for Toolhouse development support",
+  title: "SuperMe UI Clone",
+  description: "UI Clone of SuperMe",
 };
 
 export default function RootLayout({
@@ -29,9 +31,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} bg-[#F7F2E7] mt-5 max-w-7xl mx-auto antialiased`}
       >
         <Header />
-        <main className="mt-5">
-          {children}
-        </main>
+        <main className="mt-5">{children}</main>
+        <Link
+          target="_blank"
+          href="https://github.com/sahilahluwalia/superme-ui-clone"
+          className="absolute top-5 right-5 p-2 bg-white rounded-full hover:scale-110 duration-300"
+        >
+          <FaGithub className="text-gray-700 sm:size-10 size-5" />
+        </Link>
       </body>
     </html>
   );
